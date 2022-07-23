9c3a6225c1fec62643180d54c976d3db03f3f885
Frederik <frederikju@gmail.com>
2022-07-23 13:52:57 +0200

    Added JFrame code

--- JavaProj/src/Main.java
+++ JavaProj/src/Main.java
@@ -1,7 +1,35 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Se så at komme i gang!");
+		JLabel label = new JLabel();
+		ImageIcon image1 = new ImageIcon("FredTheChamp.png");
+		label.setText("Mit navn er Frederik");
+		label.setIcon(image1);
+		label.setHorizontalTextPosition(0);
+		
+		
+		
+		
+		JFrame frame = new JFrame();
+		frame.setTitle("Jframe title");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setSize(420, 420);
+		frame.getContentPane().setBackground(Color.WHITE);
+		
+		frame.add(label);
+		
+		
+		//ImageIcon image = new ImageIcon("FredTheChamp.png");
+		//frame.setIconImage(image.getImage()); //Picture location is project folder
+		
+		
 	}
 
 }
